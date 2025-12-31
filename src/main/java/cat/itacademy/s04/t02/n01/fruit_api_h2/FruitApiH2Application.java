42cda625acccce29e471fbfe8df404949dbb61db
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-31 16:05:56 +0100

    chore: create MVC package structure

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitApiH2Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitApiH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitApiH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitApiH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitApiH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
