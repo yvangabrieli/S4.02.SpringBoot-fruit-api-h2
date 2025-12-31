8a96dec78038c1d419535671d8afec27b861976a
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-31 16:51:02 +0100

    feat: add FruitRepository using Spring Data JPA

--- src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/repository/FruitRepository.java
@@ -1,7 +1,7 @@
 package cat.itacademy.s04.t02.n01.fruit_api_h2.repository;
 
-import cat.itacademy.s04.t02.n01.fruit_api_h2.model.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.model.Fruit;
 
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
 }
