8d0520bb78c5209e5d70db2c9dff02f08ed46fde
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-31 16:29:30 +0100

    feat: add Fruit JPA entity

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/model/Fruit.java
@@ -0,0 +1,26 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+
+public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int weightInKilos;
+
+    protected Fruit() {
+    }
+
+    public Fruit(String name, int weightInKilos) {
+        this.name = name;
+        this.weightInKilos = weightInKilos;
+    }
+}
+
+
