0dba1e0bc56557aad10de4269e9a3dfe037a4793
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-02 01:34:07 +0100

    feat: Add FruitController with endpoints for create, read, update, delete"

--- src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/controllers/FruitController.java
@@ -12,19 +12,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-
 @RestController
 @RequestMapping("/fruits")
 public class FruitController {
     private final FruitService service;
 
-    public FruitController (FruitService service){
+    public FruitController(FruitService service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity<FruitResponseDTO> createFruit(@Valid @RequestBody FruitRequestDTO dto){
-        Fruit fruit = new Fruit (dto.getName(), dto.getWeightInKilos());
+    public ResponseEntity<FruitResponseDTO> createFruit(@Valid @RequestBody FruitRequestDTO dto) {
+        Fruit fruit = new Fruit(dto.getName(), dto.getWeightInKilos());
         Fruit saved = service.createFruit(fruit);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -38,13 +37,13 @@ public class FruitController {
     public List<FruitResponseDTO> getALl() {
         return service.getAllFruits()
                 .stream()
-                .map(f-> new FruitResponseDTO(
+                .map(f -> new FruitResponseDTO(
                         f.getId(), f.getName(), f.getWeightInKilos()))
                 .toList();
     }
 
-    @GetMapping ("/{id}")
-    public FruitResponseDTO getById (@PathVariable Long id){
+    @GetMapping("/{id}")
+    public FruitResponseDTO getById(@PathVariable Long id) {
         Fruit fruit = service.getFruitById(id);
         return new FruitResponseDTO(
                 fruit.getId(),
@@ -53,20 +52,20 @@ public class FruitController {
     }
 
     @PutMapping("/{id}")
-    public FruitResponseDTO update (
+    public FruitResponseDTO update(
             @PathVariable Long id,
             @Valid @RequestBody FruitRequestDTO dto) {
         Fruit updated = service.updateFruit(
                 id,
-                new Fruit (dto.getName(), dto.getWeightInKilos()));
+                new Fruit(dto.getName(), dto.getWeightInKilos()));
         return new FruitResponseDTO(
                 updated.getId(),
                 updated.getName(),
                 updated.getWeightInKilos());
     }
 
-    @DeleteMapping ("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.deleteFruit(id);
         return ResponseEntity.noContent().build();
     }
