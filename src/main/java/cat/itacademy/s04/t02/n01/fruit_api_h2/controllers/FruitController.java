38e8eff6f6538b90a8cd01d7f0545584ac6543be
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-02 01:29:43 +0100

    WIP: saving local changes before rebase

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/controllers/FruitController.java
@@ -0,0 +1,73 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.controllers;
+
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitRequestDTO;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitResponseDTO;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.model.Fruit;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.services.FruitService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/fruits")
+public class FruitController {
+    private final FruitService service;
+
+    public FruitController (FruitService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<FruitResponseDTO> createFruit(@Valid @RequestBody FruitRequestDTO dto){
+        Fruit fruit = new Fruit (dto.getName(), dto.getWeightInKilos());
+        Fruit saved = service.createFruit(fruit);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new FruitResponseDTO(
+                        saved.getId(),
+                        saved.getName(),
+                        saved.getWeightInKilos()));
+    }
+
+    @GetMapping
+    public List<FruitResponseDTO> getALl() {
+        return service.getAllFruits()
+                .stream()
+                .map(f-> new FruitResponseDTO(
+                        f.getId(), f.getName(), f.getWeightInKilos()))
+                .toList();
+    }
+
+    @GetMapping ("/{id}")
+    public FruitResponseDTO getById (@PathVariable Long id){
+        Fruit fruit = service.getFruitById(id);
+        return new FruitResponseDTO(
+                fruit.getId(),
+                fruit.getName(),
+                fruit.getWeightInKilos());
+    }
+
+    @PutMapping("/{id}")
+    public FruitResponseDTO update (
+            @PathVariable Long id,
+            @Valid @RequestBody FruitRequestDTO dto) {
+        Fruit updated = service.updateFruit(
+                id,
+                new Fruit (dto.getName(), dto.getWeightInKilos()));
+        return new FruitResponseDTO(
+                updated.getId(),
+                updated.getName(),
+                updated.getWeightInKilos());
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.deleteFruit(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FruitNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(FruitNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<String> handleBusiness(BusinessRuleException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(e -> errors.put(e.getField(), e.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
