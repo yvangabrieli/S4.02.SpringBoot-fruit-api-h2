964e0cc319589e750b9695be1adad1cb51096db5
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-11 15:52:06 +0100

    fix: Added Method existsByName()

--- src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/repository/FruitRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import cat.itacademy.s04.t02.n01.fruit_api_h2.model.Fruit;
 
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
+    boolean existsByName(String name);
 }
