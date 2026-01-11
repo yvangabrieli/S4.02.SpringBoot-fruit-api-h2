a5f00b3838fb96acca93f6b7e988c55516fb4285
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-11 12:55:14 +0100

    fix: Add Table name to the Entity

--- src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/model/Fruit.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/model/Fruit.java
@@ -4,9 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "fruits")
 @Getter
 @Setter
-
 public class Fruit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
